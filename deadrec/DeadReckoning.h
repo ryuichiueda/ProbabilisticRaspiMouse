1134132f27163b0d89f5139dccb3bb9c8ea79df3
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-08-06 01:44:29 +0000

    Modify a bit

--- deadrec/DeadReckoning.h
+++ deadrec/DeadReckoning.h
@@ -6,9 +6,7 @@
 using namespace std;
 
 struct Pose{
-	double x_mm;
-	double y_mm;
-	double t_rad;
+	double x_mm, y_mm, t_rad;
 };
 
 class DeadReckoning
@@ -17,7 +15,7 @@ public:
 	DeadReckoning(double init_x_mm, double init_y_mm, double init_t_deg);
 	~DeadReckoning();
 
-	void motionUpdate(double t_delta_deg,double fw_delta_mm);
+	void motionUpdate(double t_delta_deg, double fw_delta_mm);
 	void print(ofstream *ofs);
 private:
 	Pose m_pose;
